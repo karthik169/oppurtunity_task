fbbe3addf751924007f2402b6d648b97e11869a6
KAMANI VENKATA KARTHIK <98517925+karthik169@users.noreply.github.com>
2026-05-05 09:51:40 +0530

    Update config.py

--- qatar_admin/config.py
+++ qatar_admin/config.py
@@ -1,6 +1,10 @@
 import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SECRET_KEY = 'secretkey123'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'secretkey123')
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
